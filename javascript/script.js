deaf08090c035f48520b3f95135210b591a69355
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-29 17:27:37 +0100

    commiting project content to my feature/content branch

---
+++ javascript/script.js
@@ -0,0 +1,38 @@
+document.getElementById('add-task').addEventListener('click', () => {
+  const inputTask = document.getElementById('input-value')
+  const toDoTask = document.getElementById('list')
+
+  const taskText = inputTask.value.trim()
+  if (taskText === '') {
+    alert('enter valid task')
+    return
+  }
+
+  const newTask = document.createElement('li')
+  const taskSpan = document.createElement('span')
+  const listBtn = document.createElement('button')
+
+  taskSpan.className = 'progress'
+  taskSpan.textContent = taskText
+  listBtn.textContent = 'delete'
+
+  taskSpan.addEventListener('click', () => {
+    newTask.classList.toggle('task completed')
+  })
+
+  newTask.appendChild(taskSpan)
+  toDoTask.appendChild(newTask)
+  taskSpan.appendChild(listBtn)
+  list.appendChild(newTask)
+
+  listBtn.addEventListener('click', () => {
+    list.removeChild(newTask)
+  })
+
+  listBtn.textContent = 'Done'
+  listBtn.addEventListener('click', () => {
+    list.strike(newTask.textContent)
+  })
+
+  inputTask.value = ''
+})
